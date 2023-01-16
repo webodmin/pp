1d02561722816bfa0d092e290f634b319114638d
webodmin <webodmin@gmail.com>
2023-01-16 23:05:41 +0200

    MPI method #1 minor improvements

--- lab08/lab08_1.cpp
+++ lab08/lab08_1.cpp
@@ -1,40 +1,39 @@
 #include <iostream>
 #include <mpi.h>
-#include <vector>
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv) {
+    const int array_size = 1000;
     int size, rank;
+    int data[array_size];
+    int local_sum, global_sum;
+
+    // Initialize the data array
+    for (int i = 0; i < array_size; i++) {
+        data[i] = i;
+    }
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    const int arraySize = 10000;
-    const int chunkSize = arraySize / size;
-    int array[arraySize];
-
-    if (rank == 0) {
-        for (int i = 0; i < arraySize; i++) {
-            array[i] = i;
-        }
+    // Calculate the local sum of the portion of the array assigned to this process
+    int start = rank * array_size / size;
+    int end = (rank + 1) * array_size / size;
+    local_sum = 0;
+    for (int i = start; i < end; i++) {
+        local_sum += data[i];
     }
 
-    vector<int> chunks(chunkSize);
+    // For debug
+    // cout << "Process with rank " << rank << ", start = " << start << ", end = " << end << ", local_sum = " << local_sum << endl;
 
-    cout << "Hello from process #" << rank << endl;
-    MPI_Scatter(array, chunkSize, MPI_INT, &chunks[0], chunkSize, MPI_INT, 0, MPI_COMM_WORLD);
-
-    long sum = 0;
-    for (int i = 0; i < chunkSize; i++) {
-        sum += chunks[i];
-    }
-
-    long globalSum;
-    MPI_Reduce(&sum, &globalSum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // Use MPI_Reduce to calculate the global sum
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        cout << "Sum of array elements is: " << globalSum << endl;
+        std::cout << "The sum of the elements in the array with size " << array_size << " is: " << global_sum << std::endl;
     }
 
     MPI_Finalize();
